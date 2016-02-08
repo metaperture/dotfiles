6dfea66d132cd66ad3ed8e899384ab90f5d03b18
Michael <metaperture@users.noreply.github.com>
2016-02-08 12:45:10 -0500

    Create bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,5 @@
+
+# install conda
+curl -L --proxy-negotiate -U : -O https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
+bash ./Miniconda3-latest-Linux-x86_64.sh -b -p "$HOME/.miniconda" # in batch mode: install to $HOME/.miniconda3
+rm ./Miniconda3-latest-Linux-x86_64.sh
