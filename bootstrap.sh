4a5e929180a0110f84ab8b86a3aeb290266043f5
Michael <metaperture@users.noreply.github.com>
2016-02-08 13:56:54 -0500

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,7 @@ curl -L --proxy-negotiate -U : -O https://repo.continuum.io/miniconda/Miniconda3
 bash ./Miniconda3-latest-Linux-x86_64.sh -b -p "$HOME/.miniconda" # in batch mode: install to $HOME/.miniconda3
 rm ./Miniconda3-latest-Linux-x86_64.sh
 export PATH=$HOME/.miniconda/bin/:$PATH
-bash -e "$CMDPREFIX pip install dotfiles" # CMDPREFIX might = gssproxy2 -e
+eval "$CMDPREFIX pip install dotfiles" # CMDPREFIX might = gssproxy2 -e
 THISDIR=$(readlink -f $(dirname $0))
 cp $THISDIR/dotfilesrc ~/.dotfilesrc
 dotfiles --sync --force
